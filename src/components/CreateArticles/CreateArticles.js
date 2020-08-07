ff7f338ac923824f4842485f4588d16eb95f5ab4
JanithaGit <janithaprashadkatukenda@gmail.com>
2020-08-07 12:27:59 +0530

    First commit

---
+++ src/components/CreateArticles/CreateArticles.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './CreateArticles.css';
+
+const CreateArticles =() =>{
+
+    return(
+<div className={classes.Body}>
+<h1>Create An Article</h1>
+
+
+</div>
+
+    )
+}
+
+export default CreateArticles;
---
+++ src/views/CreateArticle/CreateArticle.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import CreateArticles from '../../components/CreateArticles/CreateArticles';
+
+class CreateArticle extends Component {
+
+    render(){
+
+        return(
+            <CreateArticles/>
+        )
+    }
+}
+
+export default CreateArticle;
